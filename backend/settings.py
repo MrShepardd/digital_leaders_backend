de705073044e6b403c6539dec82f0a16092ccff3
Konstantin Travin <49562463+MrShepardd@users.noreply.github.com>
2020-10-10 19:45:14 +1000

    update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -6,9 +6,9 @@ SECRET_KEY = '67^1+f=7(d-+t!_+vvsdd(_w1h3+-qa9ksyq=cyfzl_21hsai%'
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['10.62.23.50', '23.111.204.120', 'okboomers.top', 'localhost', '10.101.6.124', 'srv-scad01.dvfu.ru', 'science.dvfu.ru']
+ALLOWED_HOSTS = ['45.14.50.89', 'localhost']
 
-CSRF_TRUSTED_ORIGINS = ['10.62.23.50', '23.111.204.120', 'okboomers.top', 'localhost', '10.101.6.124', 'srv-scad01.dvfu.ru', 'science.dvfu.ru']
+CSRF_TRUSTED_ORIGINS = ['45.14.50.89', 'localhost']
 
 USE_X_FORWARDED_HOST = True
 
@@ -89,18 +89,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# CORS_ORIGIN_ALLOW_ALL = True # If this is used then `CORS_ORIGIN_WHITELIST` will not have any effect
-CORS_ALLOW_CREDENTIALS = True
-CORS_ORIGIN_WHITELIST = [
-    'http://localhost:443',
-] # If this is used, then not need to use `CORS_ORIGIN_ALLOW_ALL = True`
-CORS_ORIGIN_REGEX_WHITELIST = [
-    'http://localhost:443',
-]
-
 CRONJOBS = [
-    ('0 0 */14 * *', 'api.cron.download_all_fefu_documents'),
-    ('0 1 */14 * *', 'api.cron.download_all_citations'),
-    ('0 10 */14 * *', 'api.cron.download_all_authors'),
-    ('0 20 */14 * *', 'api.cron.refill_db'),
+    ('0 7 */14 * *', 'api.cron.download_all_district'),
+    ('5 7 */14 * *', 'api.cron.download_all_stops'),
+    ('10 7 */14 * *', 'api.cron.download_all_atm'),
+    ('15 7 */14 * *', 'api.cron.refill_db'),
 ]
